1efd32803f3f33cabd29cddc32ed9c9734e2e0c8
pltanhthu <pltanhthu@gmail.com>
2016-03-07 17:25:42 +0000

    delete old codes

--- src/main/java/org/insight_centre/urq/citypulse/wp5/contextual_filter/data/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.insight_centre.urq.citypulse.wp5.contextual_filter.data;
-
-public class Constants {
-	public static final String EVENTSESSION = "EVENTSESSION";
-	public static final String APPLICATIONSESSION = "APPLICATIONSESSION";
-
-}
